cd304e04e854040ad8faf57e5a0f6f1ef4f21d9c
Diego-mac <diego.agudelo@industries-hf.com>
2024-09-29 16:41:42 -0500

    First commit

---
+++ src/main/java/com/industrieshf/springboot/authmicroservice/springboot_authmicroservice/SpringbootAuthmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.industrieshf.springboot.authmicroservice.springboot_authmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAuthmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAuthmicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/industrieshf/springboot/authmicroservice/springboot_authmicroservice/SpringbootAuthmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.industrieshf.springboot.authmicroservice.springboot_authmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAuthmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
